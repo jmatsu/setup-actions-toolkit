63ab787a7ad26c14106293d334602f7d50bd1d4d
Jumpei Matsuda <jmatsu.drm@gmail.com>
2021-04-07 19:07:16 +0900

    Updated available commands

--- .github/bin/verify_executable_commands.bash
+++ .github/bin/verify_executable_commands.bash
@@ -10,9 +10,7 @@ which log-error
 which log-warning
 which resume-commands
 which set-env
-which export-env
 which set-output
 which set-secret
-which add-mask
 which start-group
 which stop-commands
